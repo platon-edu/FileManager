7c637c74a89af81d21a5ab258d83de01b6736e7c
platon-p <platonpechenev@ya.ru>
2022-05-24 17:39:02 +0300

    Update

--- Dirs/Program.cs
+++ Dirs/Program.cs
@@ -7,75 +7,102 @@ namespace Dirs
 {
     class Program
     {
-        private static string message = "";
-        private static string[] argss = { };
-
         // https://docs.google.com/document/d/1I1ct-BK_XRhGlXDqZ6_ZyFCBjYIU7QBzwGwHQSUeAt0/edit
+
+        private static char separator = '/';
+
         static void Main(string[] args)
         {
+            if (OperatingSystem.IsWindows()) separator = '\\';
             Console.Clear();
             Console.WriteLine();
             PrintDir(Directory.GetCurrentDirectory());
             Cycle();
         }
 
-        public static void Cycle()
+        private static void Cycle()
         {
+            string message = "";
+            string[] argss;
             while (true)
             {
                 if (message == "")
                 {
-                    Console.Clear();
                     PrintDir(Directory.GetCurrentDirectory());
                 }
                 else
                     Console.WriteLine(message);
+
                 Console.Write("-> ");
                 string command = Console.ReadLine();
                 argss = command.Split(' ');
-                if (argss[0] == "cd")
+                if (argss[0] == "exit")
                 {
-                    message = cd(argss[1]);
+                    Console.WriteLine("Пока...");
+                    break;
                 }
-                else if (argss[0] == "touch")
-                    message = touch(argss[1]);
-                else if (argss[0] == "removef")
-                {
-                    if (argss[1].Contains('.')) File.Delete(Directory.GetCurrentDirectory());
-                }
+                if (argss[0] == "cd") message = Cd(argss[1]);
+                else if (argss[0] == "touch") message = Touch(argss[1]);
+                else if (argss[0] == "md") message = MakeDir(argss[1]);
+                else if (argss[0] == "removeF") message = RemoveFile(argss[1]);
+                else if (argss[0] == "removeD") message = RemoveDir(argss[1]);
                 else message = "Команда не найдена";
             }
         }
 
-        public static string touch(string filename)
+        private static string Touch(string fileName)
         {
-            if (File.Exists(Directory.GetCurrentDirectory() + "\\" + filename)) return "Файл уже существует";
-            File.Create(Directory.GetCurrentDirectory() + "\\" + filename);
+            if (File.Exists(fileName)) return "Файл уже существует";
+            File.Create(fileName);
             return "";
         }
 
-        public static string cd(string dirr)
+        private static string MakeDir(string dirName)
+        {
+            if (Directory.Exists(dirName)) return "Директория уже существует";
+            Directory.CreateDirectory(dirName);
+            return "";
+        }
+
+        private static string RemoveDir(string dirName)
+        {
+            if (!Directory.Exists(dirName)) return "Такой директории нет";
+            Directory.Delete(dirName);
+            return "";
+        }
+
+        private static string RemoveFile(string fileName)
+        {
+            if (!File.Exists(fileName)) return "Такого файла нет";
+            Directory.Delete(fileName);
+            return "";
+        }
+
+
+        private static string Cd(string dirr)
         {
             if (dirr == "..")
             {
-                Directory.SetCurrentDirectory(string.Join('\\',
-                    Directory.GetCurrentDirectory().Split('\\')[..^1]));
+                Directory.SetCurrentDirectory(string.Join(separator,
+                    Directory.GetCurrentDirectory().Split(separator)[..^1]));
                 return "";
             }
 
             if (dirr == ".")
                 return "";
-            string dd = Directory.GetCurrentDirectory() + "\\" + dirr;
+            string dd = Directory.GetCurrentDirectory() + separator + dirr;
             if (Directory.Exists(dd))
             {
                 Directory.SetCurrentDirectory(dd);
                 return "";
             }
+
             return "Директория не найдена";
         }
 
-        static void PrintDir(string dirName)
+        private static void PrintDir(string dirName)
         {
+            Console.Clear();
             Console.WriteLine("╔" + new string('═', dirName.Length + 2) + "╗");
             Console.WriteLine("║ " + dirName + " ║");
             Console.WriteLine('╠' + new string('═', Console.WindowWidth / 2 - 2) + '╦' +
@@ -85,16 +112,15 @@ namespace Dirs
             // foreach (var i in files) Console.WriteLine(i);
             for (int i = 0; i < Math.Max(files.Length, dirrs.Length); i++)
             {
-                PrintLine(i < dirrs.Length ? dirrs[i].Split('\\')[^1] : "",
-                    i < files.Length ? files[i].Split('\\')[^1] : "");
+                PrintLine(i < dirrs.Length ? dirrs[i].Split(separator)[^1] : "",
+                    i < files.Length ? files[i].Split(separator)[^1] : "");
             }
 
             Console.WriteLine('╚' + new string('═', Console.WindowWidth / 2 - 2) + '╩' +
                               new string('═', Console.WindowWidth / 2 - 2) + '╝');
-            if (message != "") Console.WriteLine(message);
         }
 
-        static void PrintLine(string d, string f)
+        private static void PrintLine(string d, string f)
         {
             Console.Write("║  ");
             Console.Write(d);
